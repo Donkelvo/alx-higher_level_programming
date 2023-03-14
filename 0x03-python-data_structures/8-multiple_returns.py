b90b23ed10fa0e55fbeaa2bca58a7d7ce9903964
donkelvo <elejeokechukwu@gmail.com>
2023-03-14 12:56:17 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        list = [len(sentence), None]
-        new_tuplex = tuple(list)
-    else:
-        list = [len(sentence), sentence[0]]
-        new_tuplex = tuple(list)
-    return new_tuplex
-
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
