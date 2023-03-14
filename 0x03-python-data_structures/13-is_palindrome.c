3d24da7a20fbee469ab2f125b1393e0926599db2
donkelvo <elejeokechukwu@gmail.com>
2023-03-14 13:12:09 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- *is_palindrome - ispalindrome
- *@head: list
- *Return: number elements.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int is_palindrome(listint_t **head)
+int main(void)
 {
+	listint_t *head;
+
 	return (0);
 }
